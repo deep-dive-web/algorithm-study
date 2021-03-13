e07f2e5ced2976156f6bf3fbd195e8105db7c059
kwonhygge <cleancode.kwon@gmail.com>
2021-03-14 00:55:34 +0900

    Add: 프로그래머스 - K번째 수

---
+++ hyunkyung/sorting/KNumber.js
@@ -0,0 +1,9 @@
+function solution(array, commands) {
+    const answer = commands.map(command =>{
+        const newArray = array
+            .slice(command[0]-1,command[1])
+            .sort((a,b) => {return a-b});
+        return newArray[command[2]-1];
+    } )
+    return answer;
+}

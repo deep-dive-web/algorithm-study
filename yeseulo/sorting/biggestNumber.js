237532d8cb4052989a623091337f38370bc38d9d
Yeseul O <yeseulo@gmail.com>
2021-03-14 10:59:28 +0900

    Fix typo

--- yeseulo/sorting/biggestNumber.js
+++ yeseulo/sorting/biggestNumber.js
@@ -1,12 +1,12 @@
 /**
- * 프로그래머스 - K번째 수
+ * 프로그래머스 - 가장 큰 수
  * @param {number[]} numbers
  * @return {string}
  */
 function solution(numbers) {
   const answer = numbers
     .map((item) => item + "")
-    .sort((a, b) => b + a - (a + b))
+    .sort((a, b) => (b + a) - (a + b))
     .join("");
 
   return answer[0] === "0" ? "0" : answer;

28f01a7442a0ef091b0230beacdf35510e4035c6
sujin-park <psujin831@gmail.com>
2021-03-18 19:24:08 +0900

    Add: H-Index 풀이 추가

--- sujin-park/sorting/HIndex.js
+++ sujin-park/sorting/HIndex.js
@@ -14,3 +14,20 @@ function solution(citations) {
     
     return result;
 }
+
+/**
+ * 프로그래머스 - H-Index
+ * @param {number[]} citations 
+ * @return {number}
+ */
+function solution2(citations) {
+    let result = 0;
+
+    citations.sort((a, b) => b - a);
+    citations.forEach((citation, index) => {
+        const count = index + 1;
+        if (citation >= count) result = count;
+    })
+    
+    return result;
+}

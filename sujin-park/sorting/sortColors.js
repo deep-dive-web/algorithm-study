bdd889796a699e8811432c3decf8b0e4e63ba3fe
sujin-park <psujin831@gmail.com>
2021-03-23 20:23:40 +0900

    Update: change sort colors without sort library

--- sujin-park/sorting/sortColors.js
+++ sujin-park/sorting/sortColors.js
@@ -4,5 +4,22 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function(nums) {
-  nums.sort((a, b) => a - b);
+  let left = 0;
+  let right = nums.length - 1;
+  let current = left;
+  
+  const swap = (i, j) => [nums[i], nums[j]] = [nums[j], nums[i]]
+  
+  while(current <= right) {
+      if(nums[current] === 0) {
+          swap(left, current);
+          left++;
+          current++;
+      } else if(nums[current] === 2) {
+          swap(right, current);
+          right--;
+      } else {
+          current++;
+      }
+  }
 };

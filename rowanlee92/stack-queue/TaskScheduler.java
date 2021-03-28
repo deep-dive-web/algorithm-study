48d90812320d170e7465d4ca33376fc905f639c1
rowanlee92 <watrv41@gmail.com>
2021-03-28 23:10:30 +0900

    Update: TaskScheduler

--- rowanlee92/stack-queue/TaskScheduler.java
+++ rowanlee92/stack-queue/TaskScheduler.java
@@ -1,8 +1,9 @@
 package com.janjanee.algo.datastructure;
 
 public class TaskScheduler {
-    public int leastInterval(char[] tasks, int n) {
+    public static int leastInterval(char[] tasks, int n) {
 
+        // 최대 작업 숫자, 최대 작업 숫자를 가진 동일한 작업의 수 구하기
         int[] counter = new int[26];
         int max = 0;
         int maxCount = 0;
@@ -17,12 +18,18 @@ public class TaskScheduler {
             }
         }
 
+        // ? 그룹 개수
         int partCount = max - 1;
+        // ? 그룹 내의 ? 개수
         int partLength = n - (maxCount - 1);
+        // ? 의 총 개수
         int emptySlots = partCount * partLength;
+        // 남아있는 잔여 작업 개수
         int availableTasks = tasks.length - max * maxCount;
+        // 유휴 시간 개수 (max 함수를 이용하여 idle 값이 존재하는 양수일 경우만 계산하도록 한다.)
         int idles = Math.max(0, emptySlots - availableTasks);
 
+        // 작업의 길이 + 유휴시간
         return tasks.length + idles;
     }
 

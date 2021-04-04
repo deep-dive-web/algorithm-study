ff6f9a846b724e32ac6dd0c17dac6204df0f0427
Yeseul O <yeseulo@gmail.com>
2021-04-04 09:40:28 +0900

    Add: Heap, Tree

---
+++ yeseulo/heap-tree/topKfrequentElements.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function (nums, k) {
+  const map = {};
+
+  nums.forEach((num) => {
+    map[num] = map[num] ? map[num] + 1 : 1;
+  });
+
+  const sorted = Object.entries(map).sort(
+    ([key1, val1], [key2, val2]) => val2 - val1
+  );
+
+  return sorted.slice(0, k).map((num) => num[0]);
+};

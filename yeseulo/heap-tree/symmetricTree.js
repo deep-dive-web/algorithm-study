cf5bbcc5c34c425b93d3cf89469c5b6b6f0a82a2
Yeseul O <yeseulo@gmail.com>
2021-04-04 09:40:28 +0900

    Add: Heap, Tree

---
+++ yeseulo/heap-tree/symmetricTree.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function (root) {
+  const checker = function (a, b) {
+    if (a === null && b === null) return true;
+    if (a === null || b === null) return false;
+    if (a.val !== b.val) return false;
+    return checker(a.right, b.left) && checker(a.left, b.right);
+  };
+  return checker(root.left, root.right);
+};
+
+

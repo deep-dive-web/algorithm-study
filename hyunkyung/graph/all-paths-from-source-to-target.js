121218324d2a24ba5a083c99eeeffa5288aa8fc1
kwonhygge <cleancode.kwon@gmail.com>
2021-04-17 17:40:50 +0900

    graph

---
+++ hyunkyung/graph/all-paths-from-source-to-target.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[][]} graph
+ * @return {number[][]}
+ */
+var allPathsSourceTarget = function(graph) {
+    const paths = []
+    const dfs = (index, path) => {
+        // only if the path a target path
+        if(path[path.length - 1] == graph.length - 1) {
+            paths.push(path);
+            return;
+        }
+        for(let i = 0; i < graph[index].length; i++) {
+            dfs(graph[index][i], [...path, graph[index][i]])
+        }
+    }
+    dfs(0, [0])
+    return paths
+};

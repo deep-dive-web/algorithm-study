3be11148ca79f9639d702d95924506146bf22d9f
sujin-park <psujin831@gmail.com>
2021-04-17 17:44:12 +0900

    Add: Leetcode - flood fill, find the town judge, all paths from source to target

---
+++ sujin-park/graph/findTheTownJudge.js
@@ -0,0 +1,25 @@
+/**
+ * 997. Find the Town Judge
+ * @param {number} N
+ * @param {number[][]} trust
+ * @return {number}
+ */
+const findJudge = function(N, trust) {
+  const truster = Array(N).fill(0);
+  const trustee = Array(N).fill(0);
+  
+  for(let i = 0; i < trust.length; i++) {
+    let [a, b] = trust[i];
+
+    truster[a - 1]++;
+    trustee[b - 1]++;
+  }
+  
+  for (let i = 0; i < N; i++) {
+      if (!truster[i] && trustee[i] === N - 1) {
+        return i;
+      }
+  }
+  
+  return -1;
+};

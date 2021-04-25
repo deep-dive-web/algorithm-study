ef8146fc54f578583b7651b795c0dc1c28e52c90
Yeseul O <yeseulo@gmail.com>
2021-04-25 10:24:59 +0900

    Add two sum

--- yeseulo/hash-table/two-sum.js
+++ yeseulo/hash-table/two-sum.js
@@ -4,4 +4,12 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function (nums, target) {};
+var twoSum = function (nums, target) {
+  var map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(target - nums[i])) {
+      return [map.get(target - nums[i]), i];
+    }
+    map.set(nums[i], i);
+  }
+};
